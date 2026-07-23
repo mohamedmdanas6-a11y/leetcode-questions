9cc7a567008ae808ce3b8159060463cb6936e494
mohamedmdanas6-a11y <mohamedmdanas6@gmail.com>
2026-07-23 13:51:07 +0530

    Add solution to find the first palindrome in an array

---
+++ Day 9-find first palindromi.java
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder(word);
+            if (word.equals(sb.reverse().toString())) {
+                return word;
+            }
+        }
+        return "";
+    }
+}
+
+
